3d8a02deca24570addc47214ac834b336c65c1de
chris48s <c.mshaw@yahoo.co.uk>
2016-09-13 20:26:46 +0100

    update to match new base scraper format

--- scraper.py
+++ scraper.py
@@ -1,4 +1,4 @@
-from geojson_scraper import scrape
+from dc_base_scrapers.geojson_scraper import GeoJsonScraper
 
 
 stations_url = "https://webgis1.barrowbc.gov.uk/inspire/wfs?service=WFS&version=1.3.0&request=GetFeature&typeNames=BBC%3APOLLINGSTATIONS&outputFormat=json&srsName=EPSG%3A4326"
@@ -6,5 +6,7 @@ districts_url = "https://webgis1.barrowbc.gov.uk/inspire/wfs?service=WFS&version
 council_id = 'E07000027'
 
 
-scrape(stations_url, council_id, 'utf-8', 'stations')
-scrape(districts_url, council_id, 'utf-8', 'districts')
+stations_scraper = GeoJsonScraper(stations_url, council_id, 'utf-8', 'stations')
+stations_scraper.scrape()
+districts_scraper = GeoJsonScraper(districts_url, council_id, 'utf-8', 'districts')
+districts_scraper.scrape()
